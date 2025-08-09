7dbca5c8afd6780513060d9c0f9e85e56a798e44
risonyang <risonyang@tencent.com>
2025-08-09 23:30:23 +0800

    安装 procps-ng

--- docker/build.sh
+++ docker/build.sh
@@ -1,11 +1,12 @@
 #/bin/sh
 
-docker_repo=675730607
-docker_name=rockylinux-10-ubi.arm64
-docker_tag=20250706v1
+image_repo=675730607
+image_name=rockylinux-10-ubi.arm64
+image_tag=20250809v2
 
-docker_fullname="${docker_repo}/${docker_name}:${docker_tag}"
+image_fullname="${image_repo}/${image_name}:${image_tag}"
 
-docker build --rm -t "${docker_fullname}"  -f Dockerfile .
-docker push "${docker_fullname}"
+container_cmd=docker
+${container_cmd} build --rm -t "${image_fullname}"  -f Dockerfile .
+${container_cmd} push "${image_fullname}"
 
