dedd5f089fa1077c223270d1e81c5198fcc57d8a
risonyang <risonyang@tencent.com>
2025-08-23 10:33:19 +0800

    add param_local_ssh_port

--- docker/dev/data/entry_point.sh
+++ docker/dev/data/entry_point.sh
@@ -5,8 +5,13 @@ sudo chown -R user00:users ${home_dir}
 if [ ! -f ${home_dir}/.bashrc ]; then
     cp /tmp/docker_build_data/home/bashrc ${home_dir}/.bashrc
 fi
+if [ ! -f ${home_dir}/.bash_profile ]; then
+    cp /tmp/docker_build_data/home/bash_profile ${home_dir}/.bash_profile
+fi
 if [ ! -f ${home_dir}/.vimrc ]; then
     cp /tmp/docker_build_data/home/vimrc ${home_dir}/.vimrc
 fi
+
+sudo service ssh start
 
 exec "$@"
